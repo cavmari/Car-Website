977de3a4040fcee3d466e846df7eeb4af0472f36
Mariana <117183854+cavmari@users.noreply.github.com>
2024-05-03 14:19:04 -0300

    Add files via upload

---
+++ register_admin.php
@@ -0,0 +1,94 @@
+<?php
+
+include '../components/connect.php';
+
+session_start();
+
+$admin_id = $_SESSION['admin_id'];
+
+if(!isset($admin_id)){
+   header('location:admin_login.php');
+};
+
+if(isset($_POST['submit'])){
+
+   $name = $_POST['name'];
+   $name = filter_var($name, FILTER_SANITIZE_STRING);
+   $pass = sha1($_POST['pass']);
+   $pass = filter_var($pass, FILTER_SANITIZE_STRING);
+   $cpass = sha1($_POST['cpass']);
+   $cpass = filter_var($cpass, FILTER_SANITIZE_STRING);
+
+   $select_admin = $conn->prepare("SELECT * FROM `admin` WHERE name = ?");
+   $select_admin->execute([$name]);
+   
+   if($select_admin->rowCount() > 0){
+      $message[] = 'Nome ja em uso.';
+   }else{
+      if($pass != $cpass){
+         $message[] = 'Senhas não conferem.';
+      }else{
+         $insert_admin = $conn->prepare("INSERT INTO `admin`(name, password) VALUES(?,?)");
+         $insert_admin->execute([$name, $cpass]);
+         $message[] = 'Novo adm registrado.';
+      }
+   }
+
+}
+
+?>
+
+<!DOCTYPE html>
+<html lang="en">
+<head>
+   <meta charset="UTF-8">
+   <meta http-equiv="X-UA-Compatible" content="IE=edge">
+   <meta name="viewport" content="width=device-width, initial-scale=1.0">
+   <title>Registrar</title>
+
+
+   <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/6.1.1/css/all.min.css">
+
+
+   <link rel="stylesheet" href="../css/admin_style.css">
+
+</head>
+<body>
+
+<?php include '../components/admin_header.php' ?>
+
+
+
+<section class="form-container">
+
+   <form action="" method="POST">
+      <h3>Novo Registro</h3>
+      <input type="text" name="name" maxlength="20" required placeholder="Insira seu nome" class="box" oninput="this.value = this.value.replace(/\s/g, '')">
+      <input type="password" name="pass" maxlength="20" required placeholder="Insira sua senha" class="box" oninput="this.value = this.value.replace(/\s/g, '')">
+      <input type="password" name="cpass" maxlength="20" required placeholder="Confirme sua senha" class="box" oninput="this.value = this.value.replace(/\s/g, '')">
+      <input type="submit" value="Registrar" name="submit" class="btn">
+   </form>
+
+</section>
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+<script src="../js/admin_script.js"></script>
+
+</body>
+</html>
